9c785d5bd0ae39beed38a87e46aa2567740d657b
Tim M <timothy.romero.manas@gmail.com>
2021-04-16 18:58:44 -0400

    Adding frontend for list productS'

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { listProducts } from '../actions/productActions'
+// import Product from '../components/Product'
 
 const HomeScreen = () => {
+  const dispatch = useDispatch()
+  const productList = useSelector((state) => state.productList)
+  const { products } = productList
+  console.log('Products: ' + JSON.stringify(products))
+
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch])
+
   return <h1>HomeScreen</h1>
 }
 
