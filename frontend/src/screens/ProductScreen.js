b3a01bdd131f05f3da0e85f69634c28a3b9c991d
Tim M <timothy.romero.manas@gmail.com>
2021-04-19 22:59:37 -0400

    Added ProductScreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
-import { Row, Col } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import { productDetails } from '../actions/productActions'
-import Product from '../components/Product'
 
 const ProductScreen = ({ history, match }) => {
   const dispatch = useDispatch()
@@ -15,7 +15,79 @@ const ProductScreen = ({ history, match }) => {
 
   return (
     <>
-      <h1>Product Screen</h1>
+      <Row>
+        <Col>
+          <Image
+            src={product.image}
+            alt={product.name}
+            fluid
+            style={{ width: '100%' }}
+          />
+        </Col>
+        <Col>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              {/* {product.countInStock > 0 && (
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Qty</Col>
+                    <Col>
+                      <Form.Control
+                        as='select'
+                        value={qty}
+                        onChange={(e) => setQty(e.target.value)}
+                      >
+                        {[...Array(product.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </Form.Control>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              )}
+
+              <ListGroup.Item>
+                <Button
+                  onClick={addToCartHandler}
+                  className='btn-block'
+                  type='button'
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item> */}
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
     </>
   )
 }
