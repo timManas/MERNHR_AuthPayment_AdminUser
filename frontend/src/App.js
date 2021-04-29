821d2a86c75de6627369bd722dd275f2eddbd792
Tim M <timothy.romero.manas@gmail.com>
2021-04-28 20:29:12 -0400

    adding cart screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
-          <Route path='/cart/:id' component={CartScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer />
